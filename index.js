e8d39b7a52123346e4f2ba3bf2a02afee31eed8f
Saif Ali <saifali7646@gmail.com>
2022-01-04 09:53:51 -0500

    initial release of auth route

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const helmet = require("helmet");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
+
 
 const app = express();
 
@@ -19,6 +21,7 @@ app.use(helmet());
 app.use(morgan("common"));
 
 app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(8800, ()=>{
   console.log("Backend Server Running")
