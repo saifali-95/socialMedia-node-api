cc7d775aab22cd307d9b859f269ca4f8ba142d50
Saif Ali <saifali7646@gmail.com>
2022-01-04 05:40:30 -0500

    add / and /users routes

--- index.js
+++ index.js
@@ -12,6 +12,19 @@ mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true}, () => {
   console.log('MongoDB Server Connected');
 });
 
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req, res)=>{
+  res.send("Welcome to homepage");
+});
+
+app.get("/users", (req, res)=>{
+  res.send("Welcome to users page");
+});
+
 app.listen(8800, ()=>{
   console.log("Backend Server Running")
 });
