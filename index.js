9a17cfdc2b3c309bc61e85d864e2468d49fc6bd3
Saif Ali <saifali7646@gmail.com>
2022-01-04 09:46:57 -0500

    add users route in index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const userRoute = require("./routes/users")
 
 const app = express();
 
@@ -17,13 +18,7 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-app.get("/", (req, res)=>{
-  res.send("Welcome to homepage");
-});
-
-app.get("/users", (req, res)=>{
-  res.send("Welcome to users page");
-});
+app.use("/api/users", userRoute);
 
 app.listen(8800, ()=>{
   console.log("Backend Server Running")
