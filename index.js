d6fb62312b6deb6b8f814da1709c7efe351260b4
Saif Ali <saifali7646@gmail.com>
2022-01-04 04:29:28 -0500

    connect mongoose with mongoDB

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
 const app = express();
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true}, () => {
+  console.log('MongoDB Server Connected');
+});
 
 app.listen(8800, ()=>{
   console.log("Backend Server Running")
-})
+});
+
+
+
