d714e2666a8277203981ed488d6e4965ae1a8804
Saif Ali <saifali7646@gmail.com>
2022-01-05 10:03:46 -0500

    add update user detail route

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,39 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-router.get("/", (req, res)=> {
-  res.send("hey its user route")
+//update user
+router.put("/:id", async (req, res)=> {
+  if(req.body.userId === req.params.id || req.user.isAdmin) {
+    if(req.body.password){
+      try{
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      }
+      catch {
+        return res.status(500).json(err);
+      }
+    }
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      })
+      res.status(200).json("Your update has been made")
+    }
+    catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can only update your account");
+  }
 });
+
+//delete user
+
+//get a user
+
+//follow a user
+
+//unfollow a user
 
 module.exports = router;
