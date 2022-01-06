0de01ac7a34f6e92abf8d6023f443814bb0461b3
Saif Ali <saifali7646@gmail.com>
2022-01-06 08:55:44 -0500

    add follow a user route

--- routes/users.js
+++ routes/users.js
@@ -56,6 +56,31 @@ router.get("/:id", async(req, res) => {
 })
 
 //follow a user
+router.put("/:id/follow", async(req, res) => {
+
+  if(req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+
+      //Validate if the current user is already following that specific user
+      if(!user.followers.includes(req.body.userId)) {
+        await user.updateOne({$push: {followers: req.body.userId}});
+        await currentUser.updateOne({$push: {followings: req.params.id}});
+        res.status(200).json("Successfully followed this user");
+      }
+      else {
+        res.status(403).json("You are already following this user");
+      }
+    }
+    catch (err) {
+      res.status(500).json(err)
+    }
+  } else {
+    res.status(403).json("You cannot follow yourself");
+  }
+})
+
 
 //unfollow a user
 
