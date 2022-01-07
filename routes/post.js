447f7360b9819e1ac8632797886becc1602a2981
Saif Ali <saifali7646@gmail.com>
2022-01-07 16:10:40 -0500

    add a new post route

--- routes/post.js
+++ routes/post.js
@@ -1,7 +1,18 @@
 const router = require("express").Router();
-const post = require("../models/Post");
+const Post = require("../models/Post");
 
 //Get a post
+
+router.post("/", async (req, res)=> {
+  const newPost = new Post(req.body);
+  try { 
+    const savedPost = await newPost.save();
+    return res.status(200).json(savedPost);
+  } 
+  catch(err) {  
+    return res.status(500).json(err);
+  }
+})
 
 //Update a post
 
